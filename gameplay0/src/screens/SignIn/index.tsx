1f295b523746227e585be11b11540c06330c91d9
Alisson <alissonbiagiotti1@gmail.com>
2025-09-03 12:58:43 -0300

    att
    att

--- gameplay0/src/screens/SignIn/index.tsx
+++ gameplay0/src/screens/SignIn/index.tsx
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, TextInput } from 'react-native';
+import { stylesSignIn } from './style';
 
-export default function App() {
+export function SignIn() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View style={stylesSignIn.container}>
+      <Text>Aqui será construído a tela de entrada</Text>
+      <TextInput style={{height:50,width:200,borderBottomWidth:2}} />
       <StatusBar style="auto" />
     </View>
   );
