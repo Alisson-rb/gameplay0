511b7898b6ae8fc132927dc550a6c17bc6ab9dd8
Alisson-rb <fhsilvagranja@gmail.com>
2025-09-10 12:47:36 -0300

    att
    att massa

--- gameplay0/src/screens/SignIn/index.tsx
+++ gameplay0/src/screens/SignIn/index.tsx
@@ -1,14 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View, TextInput } from 'react-native';
-import { stylesSignIn } from './style';
+import { View, Text, Image} from "react-native";
+import IllustrationImg from "../../assets/illustration.png"
 
-export function SignIn() {
-  return (
-    <View style={stylesSignIn.container}>
-      <Text>Aqui será construído a tela de entrada</Text>
-      <TextInput style={{height:50,width:200,borderBottomWidth:2}} />
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
+  export function SignIn(){
+    return(
+      <Image source={IllustrationImg}/>
+    )
+  }
